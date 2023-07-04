5347592ed6ef8fe463da46b331931a5cdcd35d20
jerumanu <emmanueljeru200@gmail.com>
2023-07-04 14:47:49 +0300

    deployments

--- moringaportal/settings.py
+++ moringaportal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY ='SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-DEBUG = env('DEBUG')
+DEBUG = False
 AUTH_USER_MODEL = 'authentication.User'
 
 ALLOWED_HOSTS= ['*']
